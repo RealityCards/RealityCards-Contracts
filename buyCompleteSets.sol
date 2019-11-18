17f771633e723047c2e89a004948e7f02f842320
Andrew Stanger <andrew@stanger.org.uk>
2019-11-18 18:38:14 +0700

    completesets- import removed, now using interfaces

--- buyCompleteSets.sol
+++ buyCompleteSets.sol
@@ -1,16 +1,27 @@
 pragma solidity 0.4.20;
 
-import "./completesets.sol";
+interface IMarket {
+    function getNumTicks() view returns(uint256);
+}
 
-contract buyCompleteSets {
-    
-    IMarket marketAddress = IMarket(0x448d1d8280844513c38b2b73bde8a539d0022954);
-    CompleteSets importedContract = CompleteSets(0x48fcc9d538b9c86ba9d35b3eb0e7f64ee2b4664f);
+interface ICompleteSets {
+    function publicBuyCompleteSets(IMarket _market, uint256 _amount) payable;
+}
 
-    function buyCompleteSet () public payable 
-    {
-    uint256 setsToBuy = msg.value / 10000;
-    importedContract.publicBuyCompleteSets.value(msg.value)(marketAddress,setsToBuy);
+contract MyContract {
     
+    uint public numTicks;
+    
+    ICompleteSets completeSets = ICompleteSets(0x48fcc9d538b9c86ba9d35b3eb0e7f64ee2b4664f);
+    IMarket market = IMarket(0x448d1d8280844513c38b2b73bde8a539d0022954); 
+    
+    function getNumTicksLocal() public {
+        numTicks = market.getNumTicks();
+    }
+    
+    function buyCompleteSets() public payable {
+        uint256 setsToBuy = msg.value / market.getNumTicks();
+        completeSets.publicBuyCompleteSets.value(msg.value)(market,setsToBuy);
     }
 }
+
