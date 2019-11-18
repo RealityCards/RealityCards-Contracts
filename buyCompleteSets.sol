16aafae136d579fa5d256d0c764cdf44ee1ccbcc
Andrew Stanger <andrew@stanger.org.uk>
2019-11-18 18:39:17 +0700

    completeSets- approve function

--- buyCompleteSets.sol
+++ buyCompleteSets.sol
@@ -8,12 +8,21 @@ interface ICompleteSets {
     function publicBuyCompleteSets(IMarket _market, uint256 _amount) payable;
 }
 
+interface ERC20 {
+    function approve(address _spender, uint256 _value) public returns (bool);
+}
+
 contract MyContract {
     
     uint public numTicks;
     
     ICompleteSets completeSets = ICompleteSets(0x48fcc9d538b9c86ba9d35b3eb0e7f64ee2b4664f);
-    IMarket market = IMarket(0x448d1d8280844513c38b2b73bde8a539d0022954); 
+    IMarket market = IMarket(0x448d1d8280844513c38b2b73bde8a539d0022954);
+    ERC20 erc20 = ERC20(0x48fcc9d538b9c86ba9d35b3eb0e7f64ee2b4664f);
+    
+    function Approve () public {
+        erc20.approve(address(this), 2^256-1);
+    }
     
     function getNumTicksLocal() public {
         numTicks = market.getNumTicks();
