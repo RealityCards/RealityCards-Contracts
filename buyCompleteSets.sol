bb48a7cdaa5cfc2b449cc8a07ee6bfabd76f00a0
Andrew Stanger <andrew@stanger.org.uk>
2019-11-22 15:12:06 +0700

    v2 upgrade

--- buyCompleteSets.sol
+++ buyCompleteSets.sol
@@ -1,39 +1,39 @@
-pragma solidity 0.4.20;
+pragma solidity 0.5.10;
 
-interface IMarket {
-    function getNumTicks() view returns(uint256);
+//ShareToken is at 0x88316706a2bfe905E2dd1bA3589811e882DD1D16
+//market is at 0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1
+
+interface IMarket { }
+
+interface ShareToken 
+
+{
+    function publicBuyCompleteSets(IMarket _market, uint256 _amount) external returns (bool)  ;
+    function publicSellCompleteSets(IMarket _market, uint256 _amount) external returns (uint256 _creatorFee, uint256 _reportingFee) ;
 }
 
-interface ICompleteSets {
-    function publicBuyCompleteSets(IMarket _market, uint256 _amount) payable;
-}
-
-interface ERC20 {
-    function approve(address _spender, uint256 _value) public returns (bool);
+interface Cash 
+{
+    function approve(address _spender, uint256 _amount) external returns (bool);
 }
 
 contract MyContract {
     
-    uint public numTicks;
+    ShareToken completeSets = ShareToken(0x88316706a2bfe905E2dd1bA3589811e882DD1D16);
+    IMarket market = IMarket(0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1);
+    Cash cash = Cash(0xa836c1D6a35A443FD6F8d5d4A9cf5b1664bF76D6);
+    // ERC20 erc20 = ERC20(0x2da4d465978981bd75bbac4c9f3bda10be0b465c);
     
-    ICompleteSets completeSets = ICompleteSets(0x48fcc9d538b9c86ba9d35b3eb0e7f64ee2b4664f);
-    IMarket market = IMarket(0x448d1d8280844513c38b2b73bde8a539d0022954);
-    //Augur.sol address: 0x990b2d2af7e87cd015a607c3a95d7622c9bbede1 [this is what the tx is sent to per metamask GUI]
-    //LegacyReputationToken address: 0x903e028cb6752c9079bcf1b6bfae182d873b4b23 [this is what works when I do it via remix]
-    //Cash.sol address: 0x2da4d465978981bd75bbac4c9f3bda10be0b465c [this is what the tx is sent to per etherscan when it originally said Augur.sol]
-    ERC20 erc20 = ERC20(0x2da4d465978981bd75bbac4c9f3bda10be0b465c);
+    // function Approve () public {
+    //     erc20.approve(0x990b2d2af7e87cd015a607c3a95d7622c9bbede1, 2^256-1);
+    // }
     
-    function Approve () public {
-        erc20.approve(0x990b2d2af7e87cd015a607c3a95d7622c9bbede1, 2^256-1);
+    function buyCompleteSets() public {
+        completeSets.publicBuyCompleteSets(market,100000000);
     }
     
-    function getNumTicksLocal() public {
-        numTicks = market.getNumTicks();
-    }
-    
-    function buyCompleteSets() public payable {
-        uint256 setsToBuy = msg.value / market.getNumTicks();
-        completeSets.publicBuyCompleteSets.value(msg.value)(market,setsToBuy);
+    function approve() public
+    {
+        cash.approve(0xe2020A4a6B0a5D6C74c358e09B2b4758b5Cdb91C,(2**256)-1);
     }
 }
-
