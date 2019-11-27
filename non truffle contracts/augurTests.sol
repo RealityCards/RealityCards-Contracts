605885a902f122b30a5a51b660e3116f4ed1f0a5
Andrew Stanger <andrew@stanger.org.uk>
2019-11-27 15:07:32 +0700

    Final commit before editing main contract

--- non truffle contracts/augurTests.sol
+++ non truffle contracts/augurTests.sol
@@ -3,7 +3,10 @@ pragma solidity 0.5.10;
 //ShareToken is at 0x88316706a2bfe905E2dd1bA3589811e882DD1D16
 //market is at 0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1
 
-interface IMarket { }
+interface IMarket 
+{
+    function getWinningPayoutNumerator(uint256 _outcome) external view returns (uint256);
+}
 
 interface ShareToken 
 
@@ -22,7 +25,8 @@ interface Cash
 contract MyContract {
     
     ShareToken completeSets = ShareToken(0x88316706a2bfe905E2dd1bA3589811e882DD1D16);
-    IMarket market = IMarket(0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1);
+    // IMarket market = IMarket(0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1); //https://v2.augur.net/#!/market?id=0xe5bCC21654b0c68892FE6E6fe86F4f93853B1AA1- OPEN 
+    IMarket market = IMarket(0xf72A93d1b0c728C58B72b008C9b2FeAb82288a13); //resolved
     Cash cash = Cash(0xa836c1D6a35A443FD6F8d5d4A9cf5b1664bF76D6);
 
     function callfaucet() public 
@@ -46,5 +50,10 @@ contract MyContract {
     function getBalance(address _owner) public view returns(uint256)
     {
        return (cash.balanceOf(_owner));
+    }
+    
+    function getWinner(uint256 _outcome) public view returns(uint256)
+    {
+        return market.getWinningPayoutNumerator(_outcome);
     }
 }
