38429161d4c433ad407fde7a37c9f4555d4a84a5
Andrew Stanger <andrew@stanger.org.uk>
2020-05-08 12:09:20 +0100

    Revert "to undo"
    This reverts commit 26239ce70d9ef0ec45fda87388aac60de08662fc.

--- contracts/Test.sol/Migrations.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity 0.6.6;
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract TEST is Ownable {
-    
-    address public owner;
-    uint public last_completed_migration;
-
-}
