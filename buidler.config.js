0faa29564d7c4c7e975e59b4734d8a640504a739
Andrew Stanger <andrew@stanger.org.uk>
2020-05-10 14:31:16 +0100

    Audit ready

--- buidler.config.js
+++ buidler.config.js
@@ -1,4 +1,7 @@
 usePlugin("@nomiclabs/buidler-truffle5");
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const mnemonic = 'defense ready lady corn other ride rapid collect avocado tongue price nut'; // pls dont steal my testnet ether 
+const kovanProviderUrl = 'https://kovan.infura.io/v3/d460ac4e71f24d869c8b75119ebe4213';
 
 // This is a sample Buidler task. To learn how to create your own go to
 // https://buidler.dev/guides/create-task.html
@@ -26,6 +29,17 @@ module.exports = {
       gas: 10000000,
       blockGasLimit: 10000000,
       gasPrice: 1
-    } 
+    } ,
+    // kovan: {
+    //   network_id: 42,
+    //   accounts: {
+    //     mnemonic: mnemonic,
+    //   },
+    //   url: kovanProviderUrl,
+    //   // provider: new HDWalletProvider(mnemonic, kovanProviderUrl, 0),
+    //   gas: 9900000, //10m is 1000000000
+    //   gasPrice: 1000000000, // 1 gwei
+    //   skipDryRun: true,
+    // },
   }
 };
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,8 +4,13 @@ var CashMockup = artifacts.require("./mockups/CashMockup.sol");
 var RealitioMockup = artifacts.require("./mockups/RealitioMockup.sol");
 
 // variables
-const marketExpectedResolutionTime = 0;
-const andrewsAddress = '0x34A971cA2fd6DA2Ce2969D716dF922F17aAA1dB0';
+var marketExpectedResolutionTime = 0;
+var andrewsAddress = '0x34A971cA2fd6DA2Ce2969D716dF922F17aAA1dB0';
+var numberOfTokens = 20;
+var templateId = 2;
+var question = 'Test 6␟"X","Y","Z"␟news-politics␟en_US';
+var arbitrator = "0xA6EAd513D05347138184324392d8ceb24C116118";
+var timeout = 86400;
 
 // KOVAN ADDRESSES
 const augurCashAddressKovan = '0x86309723166C177591960E5A9a5ecb7056564331';
@@ -23,7 +28,7 @@ module.exports = function (deployer, network) {
   } else if (network === "development") {
       deployer.deploy(CashMockup).then((deployedCash) => {
         return deployer.deploy(RealitioMockup).then((deployedRealitio) => {
-            return deployer.deploy(RealityCards, andrewsAddress, deployedCash.address, deployedRealitio.address, marketExpectedResolutionTime);
+            return deployer.deploy(RealityCards, andrewsAddress, numberOfTokens, deployedCash.address, deployedRealitio.address, marketExpectedResolutionTime, templateId, question, arbitrator, timeout);
         });
       });
     }
