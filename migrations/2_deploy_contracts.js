997f1a254af11f1717e6803280089474f4dd779a
Andrew Stanger <andrew@stanger.org.uk>
2020-05-18 13:13:42 +0100

    update URI

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,14 @@ var CashMockup = artifacts.require("./mockups/CashMockup.sol");
 var RealitioMockup = artifacts.require("./mockups/RealitioMockup.sol");
 
 // variables
-var marketExpectedResolutionTime = 0;
+var marketExpectedResolutionTime = 1590062400;
 var andrewsAddress = '0x34A971cA2fd6DA2Ce2969D716dF922F17aAA1dB0';
 var numberOfTokensTest = 2;
 var numberOfTokensMain = 2;
 var templateId = 2;
-var question = 'Test 6␟"X","Y","Z"␟news-politics␟en_US';
+var question = 'What will the bitcoin price be at noon UTC May 21st 2020? ␟"Above $10k","Below $10k"␟crypto␟en_US';
 var arbitrator = "0xd47f72a2d1d0E91b0Ec5e5f5d02B2dc26d00A14D"; //kleros 4lyfe
-var timeout = 86400;
+var timeout = 43200; // 86400 = 1 day
 
 // KOVAN ADDRESSES
 const augurCashAddressKovan = '0x86309723166C177591960E5A9a5ecb7056564331';
@@ -19,6 +19,7 @@ const realitioAddressKovan = '0x50E35A1ED424aB9C0B8C7095b3d9eC2fb791A168';
 
 // MAINNET ADDRESSES
 const daiAddressMainnet = '0x6b175474e89094c44da98b954eedeac495271d0f';
+const realitioAddressMainnet = '0x325a2e0F3CCA2ddbaeBB4DfC38Df8D19ca165b47';
 
 module.exports = async (deployer, network) => {
 
@@ -30,17 +31,13 @@ module.exports = async (deployer, network) => {
     }
     });
     
-
-
   } else if (network === "mainnet") {
-    deployer.deploy(RealitioMockup).then((deployedRealitio) => {
-      return deployer.deploy(RealityCards, andrewsAddress, numberOfTokensMain, daiAddressMainnet, deployedRealitio.address, marketExpectedResolutionTime, templateId, question, arbitrator, timeout).then(async () => {
+    deployer.deploy(RealityCards, andrewsAddress, numberOfTokensMain, daiAddressMainnet, realitioAddressMainnet, marketExpectedResolutionTime, templateId, question, arbitrator, timeout).then(async () => {
         instance = await RealityCards.deployed();
       for (i = 0; i < numberOfTokensMain; i++) {
         await instance.mintNfts("uri");
       }
       });
-    });
 
   } else if (network === "development") {
       deployer.deploy(CashMockup).then((deployedCash) => {
