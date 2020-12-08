89411f5e3d7c9f0c1fa0b09bc37c389f1c08bd1e
Andrew Stanger <andrew@stanger.org.uk>
2020-12-08 14:28:34 +0000

    ownership- mainnet proxy

--- contracts/bridgeproxies/RCOracleProxyMainnet.sol
+++ contracts/bridgeproxies/RCOracleProxyMainnet.sol
@@ -3,48 +3,69 @@ pragma solidity 0.5.13;
 import '../interfaces/IRealitio.sol';
 import '../interfaces/IRCOracleProxyXdai.sol';
 import '../interfaces/IBridgeContract.sol';
+import "@openzeppelin/contracts/ownership/Ownable.sol";
 
-////////////////////
-
-contract RCOracleProxyMainnet 
+/// @title Reality Cards Oracle Proxy- xDai side
+/// @author Andrew Stanger
+contract RCOracleProxyMainnet is Ownable
 {
     IRealitio public realitio;
     IBridgeContract public bridge; 
 
     address public oracleProxyXdaiAddress;
+    address public arbitrator;
+    uint32 public timeout;
     
     mapping (address => bytes32) public questionIds;
 
     // CONSTRUCTOR
 
     constructor(address _bridgeMainnetAddress, address _realitioAddress) public {
-        bridge = IBridgeContract(_bridgeMainnetAddress);
-        realitio = IRealitio(_realitioAddress);
+        setBridgeXdaiAddress(_bridgeMainnetAddress);
+        setRealitioAddress(_realitioAddress);
+        setArbitrator(0xd47f72a2d1d0E91b0Ec5e5f5d02B2dc26d00A14D); //kleros
+        setTimeout(86400); // 24 hours
     }
+
+    // OWNED FUNCTIONS
     
-    // INITIALISATION
-    
-    function setOracleProxyXdaiAddress(address _newAddress) external {
+    /// @dev not set in constructor, address not known at deployment
+    function setOracleProxyXdaiAddress(address _newAddress) onlyOwner external {
         oracleProxyXdaiAddress = _newAddress;
+    }
+
+    function setBridgeXdaiAddress(address _newAddress) onlyOwner public {
+        bridge = IBridgeContract(_newAddress);
+    }
+
+    function setRealitioAddress(address _newAddress) onlyOwner public {
+        realitio = IRealitio(_newAddress);
+    }
+
+    function setArbitrator(address _newAddress) onlyOwner public {
+        arbitrator = _newAddress;
+    }
+
+    function setTimeout(uint32 _newTimeout) onlyOwner public {
+        timeout = _newTimeout;
     }
     
     // POSTING QUESTION TO THE ORACLE
     
     function postQuestionToOracle(address _marketAddress, string calldata _question, uint32 _oracleResolutionTime) external {
+        require(msg.sender == address(bridge), "Not bridge");
+        require(bridge.messageSender() == oracleProxyXdaiAddress, "Not proxy");
         // hard coded values
-        // string memory _question = 'TEST PLEASE IGNORE 1␟"1","2"␟politics␟en_US';
         uint256 _template_id = 2;
-        address _arbitrator = 0xd47f72a2d1d0E91b0Ec5e5f5d02B2dc26d00A14D;
-        // uint32 _timeout = 86400;
-        uint32 _timeout = 30;
         uint256 _nonce = 0;
-        
         // post to Oracle
-        bytes32 _questionId = realitio.askQuestion(_template_id, _question, _arbitrator, _timeout, _oracleResolutionTime, _nonce);
+        bytes32 _questionId = realitio.askQuestion(_template_id, _question, arbitrator, timeout, _oracleResolutionTime, _nonce);
         questionIds[_marketAddress] = _questionId;
     }
     
-    // GETTING THE WINNER FROM THE ORACLE
+    // GETTING THE WINNER FROM THE ORACLE AND PASSING TO XDAI PROXY
+
+    /// @dev can be called by anyone
     function getWinnerFromOracle(address _marketAddress) external returns(bool) {
         bytes32 _questionId = questionIds[_marketAddress];
         bool _isFinalized = realitio.isFinalized(_questionId);
@@ -52,17 +73,12 @@ contract RCOracleProxyMainnet
         // if finalised, send result over to xDai proxy
         if (_isFinalized) {
             bytes32 _winningOutcome = realitio.resultFor(_questionId);
-            _sendWinnerToXdaiProxy(_marketAddress, uint256(_winningOutcome));
+            bytes4 _methodSelector = IRCOracleProxyXdai(address(0)).setWinner.selector;
+            bytes memory data = abi.encodeWithSelector(_methodSelector, _marketAddress, _winningOutcome);
+            bridge.requireToPassMessage(oracleProxyXdaiAddress,data,200000);
         }
         
         return _isFinalized;
     }  
-    
-    // SENDING DATA TO THE XDAI PROXY
-    function _sendWinnerToXdaiProxy(address _marketAddress, uint256 _winningOutcome) public {
-        bytes4 _methodSelector = IRCOracleProxyXdai(address(0)).setWinner.selector;
-        bytes memory data = abi.encodeWithSelector(_methodSelector, _marketAddress, _winningOutcome);
-        bridge.requireToPassMessage(oracleProxyXdaiAddress,data,200000);
-    } 
     
 }
