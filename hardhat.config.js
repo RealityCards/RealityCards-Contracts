0ad537439272c76e1c78b4bd72ea7dfd00c778fc
jonjon <jonjonclark@gmail.com>
2020-12-14 13:51:17 +0200

    Scripts and requirements for cov report

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,31 +1,31 @@
 require("@nomiclabs/hardhat-truffle5");
+require("solidity-coverage");
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-    solidity: {
-        compilers: [
-          {
-            version: "0.5.13",
-            settings: {
-              optimizer: {
-                enabled: true,
-                runs: 200,
-              },
-            },
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.13",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
           },
-        ],
+        },
       },
-      paths: {
-        artifacts: './artifactsBuidler'
-      },
+    ],
+  },
+  paths: {
+    artifacts: "./artifactsBuidler",
+  },
   networks: {
     hardhat: {
       gas: 10000000,
       blockGasLimit: 10000000,
-      gasPrice: 1
+      gasPrice: 1,
     },
-    
-  }
+  },
 };
