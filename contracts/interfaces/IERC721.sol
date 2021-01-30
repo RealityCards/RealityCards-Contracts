db6a8911bb9ff1543b3066ab50bef1e1f5e91b47
Vlad Micliuc <vladpresedinte@gmail.com>
2021-01-30 13:39:49 +0700

    Update ERC721 interface

--- contracts/interfaces/IERC721.sol
+++ contracts/interfaces/IERC721.sol
@@ -3,5 +3,6 @@ pragma solidity 0.5.13;
 interface IERC721 {
 
     function mintNft(uint256,string calldata,address) external;
-
+    function ownerOf(uint256 _tokenId) external view returns (address);
+    function transferFrom(address _from, address _to, uint256 _tokenId) external payable;
 }
