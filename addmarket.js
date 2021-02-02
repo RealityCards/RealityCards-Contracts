1bbcd73f438000f492d2571b7932afeeedcc0ab5
Andrew Stanger <andrew@stanger.org.uk>
2021-02-02 15:16:21 +0000

    udpate deploy script

--- addmarket.js
+++ addmarket.js
@@ -1,17 +1,16 @@
 //require("dotenv").config();
 
 var realityCardsFactory = artifacts.require("RCFactory");
-var factoryAddress = '0x060e1BF56e238F3263fC9870c472936EEc09CeEb';
+var factoryAddress = '0x3b557a58E5c6c4Df3e3307F9c7f5ce46472d80F7';
 
 
 // variables market specific
-var marketLockingTime = 1609405525; 
-var oracleResolutionTime = 1609405525; 
-var question = 'Test10␟"X","Y","Z"␟news-politics␟en_US';
-var slug = '10'; 
+var marketLockingTime = 1612353600; 
+var oracleResolutionTime = 1612353600; 
+var question = 'Test11␟"X","Y","Z"␟news-politics␟en_US';
 var artistAddress = "0x0000000000000000000000000000000000000000";
 var affiliateAddress = "0x0000000000000000000000000000000000000000";
-var cardAffiliateAddresses = ['0x0000000000000000000000000000000000000000','0x0000000000000000000000000000000000000000'];
+var cardAffiliateAddresses = ['0x0000000000000000000000000000000000000000'];
 
 // kovan overrides (*COMMENT OUT IF MAINNET*)
 // var marketLockingTime = 100; 
@@ -27,7 +26,7 @@ module.exports = function() {
   async function createMarket() {
     // create market
     let factory = await realityCardsFactory.at(factoryAddress);
-    console.log("CREATING XDAI MARKET");
+    console.log("CREATING MARKET");
     var transaction = await factory.createMarket(
         0,
         '0x0',
@@ -37,7 +36,6 @@ module.exports = function() {
         affiliateAddress,
         cardAffiliateAddresses,
         question,
-        slug,
       );
 
     var lastAddress = await factory.getMostRecentMarket.call(0);
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -20,6 +20,7 @@ var ambAddressXdai = '0x75Df5AF045d91108662D8080fD1FEFAd6aA0bb59'
 var ambAddressMainnet = '0x4C36d2919e407f0Cc2Ee3c993ccF8ac26d9CE64e'
 var realitioAddress = '0x325a2e0F3CCA2ddbaeBB4DfC38Df8D19ca165b47'
 var arbAddressMainnet = '0x4aa42145Aa6Ebf72e164C9bBC74fbD3788045016'
+var daiAddressMainnet = '0x6b175474e89094c44da98b954eedeac495271d0f';
 
 // UPDATE THIS AFTER STAGE 1
 var xdaiProxyAddress = '0x9e15161380f76311Ed7C33AdFF52f928Fb27D84D'
@@ -42,34 +43,42 @@ module.exports = async (deployer, network, accounts) => {
     // tell treasury about factory, tell factory about nft hub and reference
     await treasury.setFactoryAddress(factory.address)
     await factory.setReferenceContractAddress(reference.address)
-    await factory.setNftHubAddress(nfthubxdai.address)
+    await factory.setNftHubAddress(nfthubxdai.address, 0)
     // deploy xdai proxy
     await deployer.deploy(
       XdaiProxy,
       ambAddressXdai,
-      rcfactory.address,
+      factory.address,
       treasury.address
     )
     xdaiproxy = await XdaiProxy.deployed()
     // tell factory about the proxy
     await factory.setProxyXdaiAddress(xdaiproxy.address)
+
   } else if (network === 'stage2') {
     // mainnet
+    // deploy mainnet nft hub
+    await deployer.deploy(NftHubMainnet)
+    nfthubmainnet = await NftHubMainnet.deployed() 
     // deploy mainnet proxy
     await deployer.deploy(
       MainnetProxy,
       ambAddressMainnet,
       realitioAddress,
-      arbAddressMainnet
+      nfthubmainnet.address,
+      arbAddressMainnet,
+      daiAddressMainnet
     )
     mainnetproxy = await MainnetProxy.deployed()
     // set xdai proxy address
     await mainnetproxy.setProxyXdaiAddress(xdaiProxyAddress)
+
   } else if (network === 'stage3') {
     // xdai
     // set mainnet proxy address
     xdaiproxy = await XdaiProxy.deployed()
     await xdaiproxy.setProxyMainnetAddress(mainnetProxyAddress)
+
   } else if (network === 'graphTesting') {
     console.log('Local Graph Testing, whoot whoot')
 
@@ -402,6 +411,6 @@ const rent = (user, market, tokenId) => {
 
 // Most recent deployments:
 
-// Treasury: 0xbfD33bb4e15140FcdC713e00fFA16bB86C8afe00
-// Factory: 0x060e1BF56e238F3263fC9870c472936EEc09CeEb
-// Proxy mainnet: 0x9ACd4771D37bc9994410084173Bd049936c8E054
+// Treasury: 0xa02a47b0dcdB907964411af1052c38747395E08D
+// Factory: 0x3b557a58E5c6c4Df3e3307F9c7f5ce46472d80F7
+// Proxy mainnet: [none]
