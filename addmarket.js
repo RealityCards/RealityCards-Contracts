31c4e64a1b842425224b4a51e3f8439b0a3728d7
Andrew Stanger <andrew@stanger.org.uk>
2021-02-23 19:22:23 +0000

    meme market

--- addmarket.js
+++ addmarket.js
@@ -5,8 +5,8 @@ var factoryAddress = '0x6f8d1Fd9EF676ccD19046723A0eb1C20Fe217463';
 
 
 // variables market specific
-var marketLockingTime = 1614556800; 
-var oracleResolutionTime = 1614556800; 
+var marketLockingTime = 1617231600; 
+var oracleResolutionTime = 1617231600; 
 var question = 'Test11␟"X","Y","Z"␟news-politics␟en_US';
 var artistAddress = "0x0000000000000000000000000000000000000000";
 var affiliateAddress = "0x0000000000000000000000000000000000000000";
@@ -18,10 +18,12 @@ var cardAffiliateAddresses = ['0x0000000000000000000000000000000000000000'];
 
 var timestamps = [0,marketLockingTime,oracleResolutionTime];
 var tokenURIs = [
-    'https://cdn.realitycards.io/version1/ethereum/token0.json',
-    'https://cdn.realitycards.io/version1/ethereum/token1.json',
-    'https://cdn.realitycards.io/version1/ethereum/token2.json',
-    'https://cdn.realitycards.io/version1/ethereum/token3.json',
+    'https://cdn.realitycards.io/version1/meme/token0.json',
+    'https://cdn.realitycards.io/version1/meme/token1.json',
+    'https://cdn.realitycards.io/version1/meme/token2.json',
+    'https://cdn.realitycards.io/version1/meme/token3.json',
+    'https://cdn.realitycards.io/version1/meme/token4.json',
+    'https://cdn.realitycards.io/version1/meme/token5.json',
 ]; 
 
 module.exports = function() {
@@ -31,7 +33,7 @@ module.exports = function() {
     console.log("CREATING MARKET");
     var transaction = await factory.createMarket(
         0,
-        'Qma5jNTWwpxBdbj981jCiMrvAS2wmTLWiYdCBFGpBqXNn8',
+        'QmXDtbowCXhhKtKyqWh5jRzn8uP8NWyeUE3aBdhTbCeAFA',
         timestamps,
         tokenURIs,
         artistAddress,
@@ -48,4 +50,4 @@ module.exports = function() {
   createMarket();
 };
 
-// address: 0xC07034d55894e652D9b59ad380Bc72459Bc33510
+// address: 0x69aBeB96F2c62CA2bDa622Fdf315E11D5460383B
