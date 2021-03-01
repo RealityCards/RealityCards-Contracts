a4cdbf13ce3b066130a64cb6f80e46b78e545851
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-01 11:47:05 +0000

    general improvements

--- addmarket.js
+++ addmarket.js
@@ -1,27 +1,30 @@
 //require("dotenv").config();
 
 var realityCardsFactory = artifacts.require("RCFactory");
-var factoryAddress = '0x76d22B0065Ada142207E2cDce12322FB3F8c0bAA';
-
+var factoryAddress = '0xe1Ab9305DA70b865d610116163A82E1fDF6cCcFD'; //testnet on Sokol
+//var factoryAddress = '0x3b557a58E5c6c4Df3e3307F9c7f5ce46472d80F7'; //beta on xDai
 
 // variables market specific
-var marketLockingTime = 1613952000; 
-var oracleResolutionTime = 1613952000; 
-var question = 'Which card will earn the least rent this week?';
+var marketOpeningTime = 0;
+var marketLockingTime = 1614978000; 
+var oracleResolutionTime = 1614978000; 
+var ipfsHash = 'QmWVz1ZohpnHWmq3qTyKqLavHfHva82Foy7MMJ9sbW6JT7';
+var question = 'What will the price of Gamestop stock be at the closing bell on Friday 5th March?';
 var artistAddress = "0x0000000000000000000000000000000000000000";
 var affiliateAddress = "0x0000000000000000000000000000000000000000";
 var cardAffiliateAddresses = ['0x0000000000000000000000000000000000000000'];
 
 // kovan overrides (*COMMENT OUT IF MAINNET*)
-//var marketLockingTime = 100; 
-//var oracleResolutionTime = 100; 
+// var marketLockingTime = 100; 
+// var oracleResolutionTime = 100; 
 
-var timestamps = [0,marketLockingTime,oracleResolutionTime];
+var timestamps = [marketOpeningTime,marketLockingTime,oracleResolutionTime];
 var tokenURIs = [
-  'https://cdn.realitycards.io/nftmetadata/lowrent/token0.json',
-  'https://cdn.realitycards.io/nftmetadata/lowrent/token1.json',
-  'https://cdn.realitycards.io/nftmetadata/lowrent/token2.json',
-  'https://cdn.realitycards.io/nftmetadata/lowrent/token3.json',
+    'https://cdn.realitycards.io/nftmetadata/gme/token0.json',
+    'https://cdn.realitycards.io/nftmetadata/gme/token1.json',
+    'https://cdn.realitycards.io/nftmetadata/gme/token2.json',
+    'https://cdn.realitycards.io/nftmetadata/gme/token3.json',
+    'https://cdn.realitycards.io/nftmetadata/gme/token4.json',
 ]; 
 
 module.exports = function() {
@@ -31,7 +34,7 @@ module.exports = function() {
     console.log("CREATING MARKET");
     var transaction = await factory.createMarket(
         0,
-        'QmUmrtLk9W47ybMDs1Pxs8CDauXdXckaV912MXmQ4orXgH',
+        ipfsHash,
         timestamps,
         tokenURIs,
         artistAddress,
