cc0c9653da49e01e9fdabff9ef27a66d64ae0a4b
Splidge <73956628+Splidge@users.noreply.github.com>
2021-03-15 12:10:13 +0000

    correct images directory

--- EventMaker.sh
+++ EventMaker.sh
@@ -3,15 +3,15 @@
 ###################
 #### Setup ########
 ###################
-EVENT_NAME="What will happen tomorrow?"
-SRC_NAME="tomorrow"
+EVENT_NAME="When will Reality Cards publicly launch?"
+SRC_NAME="release"
 CATEGORY="other"
-SLUG="test1"
-NUMBER_OF_CARDS="2"
-CARD0="nothing"
-CARD1="everything"
-CARD2="Potato"
-CARD3=""
+SLUG="rel1"
+NUMBER_OF_CARDS="4"
+CARD0="July or later"
+CARD1="June"
+CARD2="May"
+CARD3="April or before"
 CARD4=""
 CDN="https://cdn.realitycards.io/"
 IMAGES="images/"
@@ -27,7 +27,7 @@ for ((i=0;i<$NUMBER_OF_CARDS;i++))
 do
     card='CARD'$i
     eventJSON=$eventJSON'\n    "'$i'": {'
-    eventJSON=$eventJSON'\n      "image": "'$CDN$IMAGES${!card}'.png",'
+    eventJSON=$eventJSON'\n      "image": "'$CDN$IMAGES$SRC_NAME'/'${!card// /-}'.png",'
     eventJSON=$eventJSON'\n      "name": "'${!card}'"'
     if [ $i -lt "$(($NUMBER_OF_CARDS-1))" ]
     then
@@ -49,7 +49,7 @@ card='CARD'$i
 cardJSON='{\n  "name": "'${!card}'",'
 cardJSON=$cardJSON'\n  "description": "This token represents a stake in the outcome '"'"$EVENT_NAME"'"
 cardJSON=$cardJSON' at Reality Cards, the planet'"'"'s first NFT-based prediction market",'
-cardJSON=$cardJSON'\n  "image": "'$CDN$IMAGES${!card}'.png",'
+cardJSON=$cardJSON'\n  "image": "'$CDN$IMAGES$SRC_NAME'/'${!card// /-}'.png",'
 cardJSON=$cardJSON'\n  "affiliation": "Reality Cards"'
 cardJSON=$cardJSON'\n}'
 
