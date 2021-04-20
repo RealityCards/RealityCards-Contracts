68f930e0c3ddde2f6ef9e746a55246617841f74c
Splidge <73956628+Splidge@users.noreply.github.com>
2021-04-20 18:31:59 +0100

    Update contracts/RCMarket.sol
    Co-authored-by: Jason Smythe <jason@wildcards.world>

--- contracts/RCMarket.sol
+++ contracts/RCMarket.sol
@@ -841,7 +841,7 @@ contract RCMarket is Initializable, NativeMetaTransaction, IRCMarket {
     function _collectRent(uint256 _tokenId) internal {
         uint256 _timeOfThisCollection = block.timestamp;
         address _user = ownerOf(_tokenId);
-        uint256 _foreclosureTime = treasury.foreclosureTimeUser(_user); // JS/TODO: need to get foreclosure time from treasury
+        uint256 _foreclosureTime = treasury.foreclosureTimeUser(_user);
         if (marketLockingTime <= block.timestamp) {
             _timeOfThisCollection = marketLockingTime;
         }
