4d1d6216620f9810aaa89874345fea243c8d0cb0
Andrew Stanger <andrew@stanger.org.uk>
2021-05-05 23:15:32 +0100

    when launch market

--- addmarket.js
+++ addmarket.js
@@ -1,15 +1,15 @@
 //require("dotenv").config();
 
 var realityCardsFactory = artifacts.require("RCFactory");
-//var factoryAddress = '0xe1Ab9305DA70b865d610116163A82E1fDF6cCcFD'; //testnet on Sokol
+var factoryAddress = '0x31fE0805b91c4B87CB564A47d1dB42cE8fe84bff'; //new testnet on Sokol
 //var factoryAddress = '0x3b557a58E5c6c4Df3e3307F9c7f5ce46472d80F7'; //beta on xDai
-var factoryAddress = '0x5b7477AcFa49Cc71530A1119ddbC0d3c30ac8ffE'; // unaudited.rc.io
+// var factoryAddress = '0x5b7477AcFa49Cc71530A1119ddbC0d3c30ac8ffE'; // unaudited.rc.io
 
 // variables market specific
-var marketOpeningTime = 1619521200;
-var marketLockingTime = 1619737201; 
-var oracleResolutionTime = 1619737201; 
-var ipfsHash = 'QmcCbz1XWF56Gno8DRC5d3o48yNUu1ajdqRJ3edQYU5F3w';
+var marketOpeningTime = 0;
+var marketLockingTime = 1625050800; 
+var oracleResolutionTime = 1625050800; 
+var ipfsHash = 'QmeV6pxhAYTuQcA8twd2xwpMyGzu6ajAn51EQaffyPAYzT';
 var question = 'What will the $USD Bitcoin price be at the end of 29th April 2021 UTC?␟"Below $55k","Above $55k"␟cryptocurrency␟en_US';
 var artistAddress = "0x0000000000000000000000000000000000000000";
 var affiliateAddress = "0x0000000000000000000000000000000000000000";
