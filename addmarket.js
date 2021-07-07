54b5652cc8653fb54b6a53db81d14f0720ffcce2
Splidge <73956628+Splidge@users.noreply.github.com>
2021-07-07 09:57:32 +0100

    bucks suns game

--- addmarket.js
+++ addmarket.js
@@ -2,7 +2,7 @@ const realityCardsFactory = artifacts.require("RCFactory");
 const fs = require('fs');
 let myArgs = process.argv.slice(1, 9);
 
-const SRC = "EngVsDen" // put the event name in here (the folder it is stored in)
+const SRC = "bucksun" // put the event name in here (the folder it is stored in)
 // truffle exec addmarket.js --network xdai
 
 // first part of name should match the network name truffle uses
