e160ac1d6496c03ad289b1e62f6a386def0dcae7
Splidge <anything@dchilvers.com>
2021-08-06 17:55:45 +0100

    disable etherscan plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomiclabs/hardhat-truffle5");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
-require("@nomiclabs/hardhat-etherscan");
+// require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY;
