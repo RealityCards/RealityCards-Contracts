6e9fa536c21e97e3fe4c6fa22e5cf7482c9609dc
Splidge <anything@dchilvers.com>
2021-08-11 09:58:48 +0100

    remove hardhat-tracer due to CI package problems

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,6 @@
 require("@nomiclabs/hardhat-truffle5");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
-require("hardhat-tracer");
 // require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
