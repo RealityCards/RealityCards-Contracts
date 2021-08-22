234008f0c958c9b41d8d529383dfd27b81354e9f
Splidge <anything@dchilvers.com>
2021-08-22 18:25:22 +0100

    add ownable for OpenSea customisation

--- contracts/nfthubs/RCNftHubL1.sol
+++ contracts/nfthubs/RCNftHubL1.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; // for OpenSea
 import "hardhat/console.sol";
 import "../lib/NativeMetaTransaction.sol";
 import "../interfaces/IRCNftHubL1.sol";
