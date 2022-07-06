7b4a6102046eec3da12bffa32639c13e7ef506fa
Splidge <73956628+Splidge@users.noreply.github.com>
2022-07-06 09:29:45 +0100

    rename to CardsToken.sol

--- contracts/CardsToken.sol
+++ contracts/CardsToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-import '@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol';
-import 'hardhat/console.sol';
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
+import "hardhat/console.sol";
 
-contract LCToken is ERC20PresetFixedSupply {
+contract CardsToken is ERC20PresetFixedSupply {
     constructor(
         string memory name,
         string memory symbol,
